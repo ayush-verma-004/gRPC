ca74d57c2ba4ddfe7055f4489aa4ce3c199304d8
ayush-verma-004 <ayushverma593773@gmail.com>
2025-07-17 13:42:45 +0530

    Proto understanding!

---
+++ Protobuf/protobuf/src/main/java/com/javnic/protobuf/ProtobufApplication.java
@@ -0,0 +1,13 @@
+package com.javnic.protobuf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtobufApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtobufApplication.class, args);
+	}
+
+}
