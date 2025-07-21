03c9eb17b84544e33d79fcf501959f01f69cffae
ayush-verma-004 <ayushverma593773@gmail.com>
2025-07-21 17:17:47 +0530

    My gRPC communication on microservice (Unary gRPC)

---
+++ clientSideMicroservice/src/test/java/com/javnic/clientSideMicroservice/ClientSideMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javnic.clientSideMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class ClientSideMicroserviceApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
